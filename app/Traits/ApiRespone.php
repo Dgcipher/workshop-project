2be9707143f2031e831c13b146afd753ffc780be
Engsondos <engsondosahmed1@gmail.com>
2023-01-16 00:35:30 +0200

    Edit in format in Trait-Api

--- app/Traits/ApiRespone.php
+++ app/Traits/ApiRespone.php
@@ -1,7 +1,8 @@
 <?php
 namespace app\Traits;
 trait ApiRespone{
-    public function Apirespone(bool $sucsess,string $message, $data,bool $error,int $statuscode){
+    public function ApiRespone(bool $sucsess,string $message, $data,bool $error,int $statuscode)
+    {
         return response()->json(
             [
             'success'=>$sucsess,
@@ -9,13 +10,16 @@ trait ApiRespone{
             'error'=>$error
             ], $statuscode);
     }
-    public function success(string $message,int $statuscode =200){
-        return $this->Apirespone(true,$message,[],false,$statuscode);
+    public function success(string $message,int $statuscode =200)
+    {
+        return $this->ApiRespone(true,$message,[],false,$statuscode);
     }
-    public function error(string $message,int $statuscode){
+    public function error(string $message,int $statuscode)
+    {
         return $this->Apirespone(false,$message,[],true,$statuscode);
     }
-    public function SendData(string $message, $data,int $statuscode=200){
+    public function sendData(string $message, $data,int $statuscode=200)
+    {
         return $this->Apirespone(true,$message,$data,false,$statuscode);
     }
 
