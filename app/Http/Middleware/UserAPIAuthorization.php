04c377d22019998bc81d0bfb74e18851e96bb6ee
Engsondos <engsondosahmed1@gmail.com>
2023-01-18 04:37:14 +0200

    Edit in UserApiAuthorization

--- app/Http/Middleware/UserAPIAuthorization.php
+++ app/Http/Middleware/UserAPIAuthorization.php
@@ -17,7 +17,7 @@ class UserAPIAuthorization
      * @param null $capability
      * @return mixed
      */
-    public function handle(Request $request, \Closure $next , $privacy , $capability =null )
+    public function handle(Request $request, \Closure $next , $privacy , $capability = null)
 
     {
 
@@ -32,20 +32,18 @@ class UserAPIAuthorization
         }
         $user->load('role');
         $role = $user->role;
-        if(!$role)
-        {
+        if(!$role) {
             return response()->json(['message' => 'access denined'], ResponseAlias::HTTP_UNAUTHORIZED);
         }
         $permistion = $role->permissions()->where('privacy', $privacy)->first();
-        if(!$permistion)
-        {
-            return response()->json(['message' => 'access denined'], ResponseAlias::HTTP_UNAUTHORIZED);
 
+        if(!$permistion) {
+            return response()->json(['message' => 'access denined'], ResponseAlias::HTTP_UNAUTHORIZED);
         }
+
         if (is_null($capability) || in_array($capability,(array)$permistion->capabilities)) {
             return $next($request);
         }
-
 
         return response()->json(['message' => 'Unauthorized'], ResponseAlias::HTTP_UNAUTHORIZED);
 
