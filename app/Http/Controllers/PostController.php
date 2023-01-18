8affec737f640feee2abe2db68753aa1fbd443ef
Islam M Ahmed <74732441+islam-m-ahmed@users.noreply.github.com>
2023-01-18 02:23:35 +0200

    remove empty method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,16 +20,7 @@ class PostController extends Controller
         return $this->responseJson(1,'data loaded',$posts);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+ 
     /**
      * Store a newly created resource in storage.
      *
@@ -67,16 +58,6 @@ class PostController extends Controller
         return $this->responseJson(1,'data loaded',$post);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
